3fff8d77f23296414f6e6b557aab4c192c7d6c0d
CristianoLopes <lopes144@gmail.com>
2016-07-06 23:29:34 -0300

    pano de marcas

--- games/dev_Ossain/play/js/action.js
+++ games/dev_Ossain/play/js/action.js
@@ -1,20 +1,25 @@
- var correctCards = 0;
+var correctCards = 0;
 $( init );
  
- var card1 = '<p align="center"><b>O numero um é o primeiro simbolo da lista de algarismos indo arábicos, é representado pelo simbolo "2"</b></p>';
-
 function init() {
  
+  // Set progress bar
+  document.getElementById('progress').style.width= '0%';
+
   // Reset the game
   correctCards = 0;
   $('#cardPile').html( '' );
   $('#cardSlots').html( '' );
  
   // Create the pile of shuffled cards
+  var i = 0;
   var numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+  var folhas = ["carambola", "colônia", "goiaba", "cactus", "camomila", "artemisia", "manjerona", "capim santo"];
+  var loremIpsum = "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos."
+  var folhasAux = [loremIpsum, loremIpsum, loremIpsum, loremIpsum, loremIpsum, loremIpsum, loremIpsum, loremIpsum];
   numbers.sort( function() { return Math.random() - .5 } );
  
-  for ( var i=0; i<10; i++ ) {
+  for ( i=0; i<10; i++ ) {
     $('<div></div>').data( 'number', numbers[i] ).attr( 'id', 'card'+numbers[i] ).appendTo( '#cardPile' ).draggable( {
       containment: '#content',
       stack: '#cardPile div',
@@ -22,49 +27,39 @@ function init() {
       revert: true
     } );
 
-    document.getElementById('card'+numbers[i]).style.backgroundImage = "url('images/folha" + numbers[i] + "c.png')";
+    document.getElementById('card'+numbers[i]).style.backgroundImage = "url('images/folha" + numbers[i] + ".png')";
     document.getElementById('card'+numbers[i]).style.backgroundRepeat = "no-repeat";
-    document.getElementById('card'+numbers[i]).style.backgroundSize = "78% 118%";
-  
-    /* Individually coloured cards 
-     
-    #card1.correct {
-        background: url(../images/folha1.png);
-        background-size: 78% 118%;
-        background-repeat: no-repeat;
-    }
-    #card2.correct { background: brown; }
-    #card3.correct { background: orange; }
-    #card4.correct { background: yellow; }
-    #card5.correct { background: green; }
-    #card6.correct { background: cyan; }
-    #card7.correct { background: blue; }
-    #card8.correct { background: indigo; }
-    #card9.correct { background: purple; }
-    #card10.correct { background: violet; }
+    document.getElementById('card'+numbers[i]).style.backgroundSize = "contain";
+    document.getElementById('card'+numbers[i]).style.backgroundPosition = "center";
 
-    document.getElementsByClassName('card'+numbers[i]+'.correct').style.backgroundImage = "url('images/folha" + numbers[i] + "c.png')";
-    document.getElementById('card'+numbers[i]).style.backgroundImage = "url('images/folha" + numbers[i] + "c.png')";
-    document.getElementById('card'+numbers[i]).style.backgroundRepeat = "no-repeat";
-    document.getElementById('card'+numbers[i]).style.backgroundSize = "78% 118%";
-    */
-  }
+    $("#" + 'card'+ numbers[i]).mouseover(function() {
+      var id = $(this).attr('id');
+      i = id.replace("card", "");
+      $(this).attr( 'title', folhasAux[i] )
+    });
+   }
  
   // Create the card slots
-  var words = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten' ];
   for ( var i=1; i<=10; i++ ) {
-    $('<div></div>').data( 'number', i ).attr( 'id', 'cardSlot'+i ).appendTo( '#cardSlots' ).droppable( {
+    $('<div></div>').data( 'number', i ).attr({id: 'cardSlot'+i, class: 'cardSlot'}).appendTo( '#content' ).droppable( {
       accept: '#cardPile div',
       hoverClass: 'hovered',
       drop: handleCardDrop
     } );
 
-   document.getElementById('cardSlot'+i).style.backgroundImage = "url('images/folha" + i + "p.png')";
+    document.getElementById('cardSlot'+i).style.backgroundImage = "url('images/doenca" + i + ".png')";
     document.getElementById('cardSlot'+i).style.backgroundRepeat = "no-repeat";
-    document.getElementById('cardSlot'+i).style.backgroundSize = "78% 118%";
+    document.getElementById('cardSlot'+i).style.backgroundSize = "contain";
   }
  
+  		
 }
+
+function setTopo(){
+    $(window).scrollTop(0);
+}
+
+$(window).bind('scroll', setTopo);
 
 function handleCardDrop( event, ui ) {
   var slotNumber = $(this).data( 'number' );
@@ -78,11 +73,11 @@ function handleCardDrop( event, ui ) {
   if ( slotNumber == cardNumber ) {
     ui.draggable.addClass( 'correct' );
     ui.draggable.draggable( 'disable' );
-    $('#cardDescription').html( cardNumber );
     $(this).droppable( 'disable' );
     ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
     ui.draggable.draggable( 'option', 'revert', false );
     correctCards++;
+    document.getElementById('progress').style.width= (correctCards * 10) + '%';
   } 
    
   // If all the cards have been placed correctly then display a message
